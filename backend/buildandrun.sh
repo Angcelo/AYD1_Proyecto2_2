1ba1d9ccf23735fdc0c776db8e643efae7a3078b
Angel Marroquín <43527563+Angcelo@users.noreply.github.com>
2021-06-28 16:48:51 -0600

    Update buildandrun.sh

--- backend/buildandrun.sh
+++ backend/buildandrun.sh
@@ -1,3 +1,3 @@
-sudo docker rm -f $(sudo docker ps -a -q --filter ancestor=redes_s1_2021_2/node-web-app --format="{{.ID}}")
-sudo docker build . -t redes_s1_2021_2/node-web-app
-sudo docker run -p 5050:5050 -d redes_s1_2021_2/node-web-app
+sudo -S docker rm -f $(sudo docker ps -a -q --filter ancestor=redes_s1_2021_2/node-web-app --format="{{.ID}}")
+sudo -S docker build . -t redes_s1_2021_2/node-web-app
+sudo -S docker run -p 5050:5050 -d redes_s1_2021_2/node-web-app
