f2c6143bad199ea3f854bc5361c638557c25c2be
Xiom22 <2992421290101@ingenieria.usacc.edu.gt>
2021-06-28 21:05:09 -0600

    Agregando url api

---
+++ webClient/src/app/services/usuario.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  API_URL = "http://104.198.62.172:3030/"
+
+  constructor() { }
+}
